8157a201282386d4f492c6d0d64439ff0234d656
User-Suuro <godwingalvez26@gmail.com>
2026-06-20 14:51:08 +0800

    added connection to supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'routes/root.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsApp.debugAllowBannerOverride = false;
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const Root());
 }
